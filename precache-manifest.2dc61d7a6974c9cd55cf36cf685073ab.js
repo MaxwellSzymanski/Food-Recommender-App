c518c767b97044f03de459fe378509286f628208
wout <wout.vanderlinden@student.kuleuven.be>
2020-01-09 15:16:12 +0100

    Updates

---
+++ precache-manifest.2dc61d7a6974c9cd55cf36cf685073ab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d2dbd920c21819884d9efe3a5947d81",
+    "url": "/webpageproject/index.html"
+  },
+  {
+    "revision": "d864a4ba70945c5d882f",
+    "url": "/webpageproject/static/css/main.24f123e3.chunk.css"
+  },
+  {
+    "revision": "3b50d3c8c8cf3d274541",
+    "url": "/webpageproject/static/js/2.926be184.chunk.js"
+  },
+  {
+    "revision": "d864a4ba70945c5d882f",
+    "url": "/webpageproject/static/js/main.820a62ef.chunk.js"
+  },
+  {
+    "revision": "829319ecb3571ad0213b",
+    "url": "/webpageproject/static/js/runtime-main.dddb76fe.js"
+  },
+  {
+    "revision": "511982e57db24a9378a7fdcf7fc14fa2",
+    "url": "/webpageproject/static/media/dislike.511982e5.png"
+  },
+  {
+    "revision": "783493f6872f710201adcb9b36ec7678",
+    "url": "/webpageproject/static/media/plate1.783493f6.png"
+  },
+  {
+    "revision": "f863175d56323b2b46ee45a7e2007371",
+    "url": "/webpageproject/static/media/plate2.f863175d.png"
+  },
+  {
+    "revision": "d73fa75718083927fbeb7faeec40513e",
+    "url": "/webpageproject/static/media/plate3.d73fa757.png"
+  }
+]);
